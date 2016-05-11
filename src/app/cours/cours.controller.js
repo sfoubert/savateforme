9d2c65a526a351cee7cb21d0a7d394f64bab2ac5
Sebastien Foubert <sebastien.foubert@gmail.com>
2016-05-11 22:50:52 +0200

    Implementation drag and drop

--- src/app/cours/cours.controller.js
+++ src/app/cours/cours.controller.js
@@ -6,25 +6,47 @@
     .controller('CoursController', CoursController);
 
   /** @ngInject */
-  function CoursController($scope, $log) {
+  function CoursController($scope) {
     var vm = this;
 
     vm.message = 'par seb';
 
-    $scope.models = {
-        selected: null,
-        lists: {"A": [], "B": []}
+    vm.models = {
+      selected: null,
+      lists: {
+        "A": [],
+        "B": []
+      },
+      grid: {
+        "combo1": [],
+        "combo2": [],
+        "combo3": [],
+        "combo4": []
+      }
     };
 
     // Generate initial model
     for (var i = 1; i <= 3; ++i) {
-        $scope.models.lists.A.push({label: "Item A" + i});
-        $scope.models.lists.B.push({label: "Item B" + i});
+      vm.models.lists.A.push({
+        label: "Item A" + i
+      });
+      vm.models.lists.B.push({
+        label: "Item B" + i
+      });
+    }
+
+    for (var k = 0; k < 8; k++) {
+      for (var j = 1; j <= 4; j++) {
+        vm.models.grid["combo" + j].push({
+          "id": k,
+          "list": []
+        });
+      }
     }
 
     // Model to JSON for demo purpose
     $scope.$watch('models', function(model) {
-        $scope.modelAsJson = angular.toJson(model, true);
+      vm.modelAsJson = angular.toJson(model, true);
     }, true);
 
   }
